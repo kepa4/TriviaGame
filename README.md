c65cba9efa0db2ac1859a629874f3565c709aa77
Keenan <kpautler4@gmail.com>
2018-12-20 22:28:55 -0700

    show correct or incorrect screen

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TriviaGame
+
+##unfinished
+
+click on the answer of each question
+
+you should be prompted with a right or wrong screen
+
+next question shows up after 5 seconds
+
+if you fail to answer the question after 20 seconds it results to a fail screen 
+
+for whatever reason the intervals gets weird and pretty much makes the program unusable
+
+hours of debugging could not discover the problem
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -9,58 +9,96 @@ $(document).ready(function() {
     var index = 0;
     var miss = 0;
     var pass = 0;
+    var timeout;
+    var resultTimeout;
     createQuestion(index);
 
     function createQuestion(answer) {
+      clearInterval(interval);
+      time = 20;
       var randomAnswers = [
-        `<h3 id="answer">${questions[answer].option2}</h3><h3 id="answer">${questions[answer].option1
-        }</h3><h3 id="answer">${
-          questions[answer].correct
-        }</h3><h3 id="answer">${questions[answer].option3}</h3>`,
-        `<h3 id="answer">${questions[answer].option1}</h3><h3 id="answer">${
-          questions[answer].correct
-        }</h3><h3 id="answer">${
+        `<h3 id="incorrect" >${
           questions[answer].option2
-        }</h3><h3 id="answer">${questions[answer].option3}</h3>`,
-        `<h3 id="answer">${questions[answer].correct}</h3><h3 id="answer">${
+        }</h3><h3 id="incorrect">${
           questions[answer].option1
-        }</h3><h3 id="answer">${
+        }</h3><h3 id="correct">${
+          questions[answer].correct
+        }</h3><h3 id="incorrect">${questions[answer].option3}</h3>`,
+        `<h3 id="incorrect">${questions[answer].option1}</h3><h3 id="correct">${
+          questions[answer].correct
+        }</h3><h3 id="incorrect">${
           questions[answer].option2
-        }</h3><h3 id="answer">${questions[answer].option2}</h3>`,
-        `<h3 id="answer">${questions[answer].option2}</h3><h3 id="answer">${
+        }</h3><h3 id="incorrect">${questions[answer].option3}</h3>`,
+        `<h3 id="correct">${questions[answer].correct}</h3><h3 id="incorrect">${
+          questions[answer].option1
+        }</h3><h3 id="incorrect">${
+          questions[answer].option2
+        }</h3><h3 id="incorrect">${questions[answer].option3}</h3>`,
+        `<h3 id="incorrect">${
+          questions[answer].option2
+        }</h3><h3 id="incorrect">${
           questions[answer].option3
-        }</h3><h3 id="answer">${
+        }</h3><h3 id="incorrect">${
           questions[answer].option1
-        }</h3><h3 id="answer">${questions[answer].correct}</h3>`,
+        }</h3><h3 id="correct">${questions[answer].correct}</h3>`,
       ];
-
-      $('#time').append(`<h3>${time}</h3>`);
+      $('answers').empty();
+      $('#time').append(`<h2>${time}</h2>`);
       $('#question').empty();
-      $('#question').append(`<h3>${questions[index].question}</h3>`);
+      $('#question').append(`<h2>${questions[index].question}</h2>`);
       $('#answers').append(
         randomAnswers[Math.floor(Math.random() * randomAnswers.length)],
       );
-      startTimer();
-      setTimeout(showFail, 20000);
-      index++;
+      interval = setInterval(function() {
+        time--;
+        $('#time').empty();
+        $('#time').append(`<h2>${time}</h2>`);
+      }, 1000);
+      timeout = setTimeout(showFail, 20000);
     }
+
     function showFail() {
       clearInterval(interval);
       $('#time').empty();
       $('#question').empty();
       $('#answers').empty();
-      $('#body').append(
+      $('#question').append(
         `<h2>Sorry but you didnt answer in time So you got it wrong</h2>`,
       );
       miss++;
+      index++;
+      setTimeout(function() {
+        createQuestion(index);
+      }, 5000);
     }
 
-    function startTimer() {
-      interval = setInterval(function() {
-        time--;
+    $('h3').on('click', function(e) {
+      if (e.target.id === 'incorrect') {
+        clearInterval(interval);
         $('#time').empty();
-        $('#time').append(`<h3>${time}</h3>`);
-      }, 1000);
-    }
+        $('#question').empty();
+        $('#answers').empty();
+        $('#question').append(`<h2>Sorry you got the answer Wrong</h2>`);
+        index++;
+        miss++;
+        setTimeout(function() {
+          createQuestion(index);
+        }, 5000);
+      } else {
+        clearInterval(interval);
+        $('#time').empty();
+        $('#question').empty();
+        $('#answers').empty();
+        $('#question').append(`<h2>you got the answer right</h2>`);
+        index++;
+        pass++;
+        setTimeout(function() {
+          createQuestion(index);
+        }, 5000);
+      }
+    });
   }
 });
+// Most of the logic is present in the code
+// For some reason im having trouble with the clearing the intervals
+// questions are cycled through, but get all weird 
